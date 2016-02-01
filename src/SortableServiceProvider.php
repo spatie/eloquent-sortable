6313e31017351000e0e955fa26060247aaed7117
Thijs <tdemaa@itechsolutions.nl>
2016-02-01 21:45:25 +0100

    #9

--- src/SortableServiceProvider.php
+++ src/SortableServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\EloquentSortable;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class SortableServiceProvider extends ServiceProvider
@@ -33,9 +34,11 @@ class SortableServiceProvider extends ServiceProvider
      */
     public function bootEvents()
     {
-        $this->app['events']->listen('eloquent.creating*', function ($model) {
-
-            if ($model instanceof Sortable && $model->shouldSortWhenCreating()) {
+        $this->app['events']->listen('eloquent.creating*', function (Model $model) {
+            if ($model instanceof Sortable
+                and $model->shouldSortWhenCreating()
+                and is_null($model->getAttribute($model->determineOrderColumnName()))
+            ) {
                 $model->setHighestOrderNumber();
             }
         });
