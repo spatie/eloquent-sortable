4666c0e3fd6e15cbb30231df9510d5b86b496cbf
Farhan Hadi <farhan.abdhadi@gmail.com>
2020-08-24 23:22:53 +0800

    Added eloquent sortable config file

---
+++ config/eloquent-sortable.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    /*
+     * Optionally specify which column will be used as the order column. The default is order_column.
+     */
+    'order_column_name' => env('ORDER_COLUMN_NAME', 'order_column'),
+];
